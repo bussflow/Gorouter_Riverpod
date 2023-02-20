0d3c8eb1f295dfde508cc07665a65af4dc97975e
GauravPalav <gaurav.palav@ingeniumamarine.com>
2023-02-20 21:24:50 +0530

    o

--- goroutersample/lib/home2.dart
+++ goroutersample/lib/home2.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:goroutersample/v.dart';
 
 import 'C1 copy.dart';
 import 'C1.dart';
+
+final checkboxProvider = StateProvider<bool>((ref) => false);
 
 /// The details screen
 class DetailsScreen extends StatelessWidget {
@@ -17,6 +21,52 @@ class DetailsScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Consumer(
+              builder: (BuildContext context, WidgetRef ref, Widget? child) {
+                return TextField(
+                  onChanged: (String v) {
+                    ref
+                        .read(boolProvider.notifier)
+                        .update((state) => Student(v, 2));
+                  },
+                );
+              },
+            ),
+
+            Consumer(builder: (context, ref, _) {
+              return Checkbox(
+                value: ref.watch(checkboxProvider),
+                onChanged: (value) {
+                  ref.read(checkboxProvider.notifier).state = value!;
+                  ref
+                      .read(boolProvider.notifier)
+                      .update((state) => Student(value.toString(), 2));
+                },
+              );
+            }),
+
+            // TextField(
+            //   onChanged: (String value) async {
+            //     await showDialog<void>(
+            //       context: context,
+            //       builder: (BuildContext context) {
+            //         return AlertDialog(
+            //           title: const Text('Thanks!'),
+            //           content: Text(
+            //               'You typed "$value", which has length ${value.characters.length}.'),
+            //           actions: <Widget>[
+            //             TextButton(
+            //               onPressed: () {
+            //                 Navigator.pop(context);
+            //               },
+            //               child: const Text('OK'),
+            //             ),
+            //           ],
+            //         );
+            //       },
+            //     );
+            //   },
+            // ),
             ElevatedButton(
               onPressed: () => context.go('/'),
               child: const Text('Go back to the Home screen'),
